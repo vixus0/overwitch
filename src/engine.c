e1d66ace64626638fda5b3ddf075517c7be64e40
dagargo <dagargo@gmail.com>
2022-11-13 13:53:31 +0100

    Add driver detach for interface 3

--- src/engine.c
+++ src/engine.c
@@ -647,6 +647,12 @@ ow_engine_init (struct ow_engine *engine, unsigned int blocks_per_transfer,
       ret = OW_USB_ERROR_DETACHING_DRIVER;
       goto end;
     }
+  err = ow_detach_driver (engine, 3);
+  if (err < 0)
+    {
+      ret = OW_USB_ERROR_DETACHING_DRIVER;
+      goto end;
+    }
   err = libusb_set_configuration (engine->usb.device_handle, 1);
   if (err < 0)
     {
